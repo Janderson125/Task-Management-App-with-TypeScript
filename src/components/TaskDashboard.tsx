66dcdeb08658d0918269d5cb263b17aed05a1c30
Justin Anderson <jandersonperpetualhomes@gmail.com>
2025-07-21 10:28:00 -0500

    Update TaskDashboard to fix issues

--- src/components/TaskDashboard.tsx
+++ src/components/TaskDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useTasks } from '../contexts/TaskContext'
+import './TaskDashboard.css'  // Import external CSS file
 
 // Temporary user stub for demo
 const user = { name: "Guest" }
@@ -16,26 +17,35 @@ const TaskDashboard = () => {
   }
 
   return (
-    <div style={{ padding: 20 }}>
+    <div className="task-dashboard-container">
       <h1>{user.name}'s Task Dashboard</h1>
-      {/* Removed logout button since no auth */}
-      <Link to="/tasks/new" style={{ marginLeft: 20 }}>
+      <Link to="/tasks/new" className="create-task-link">
         Create New Task
       </Link>
 
-      <ul>
+      <ul className="task-list">
         {tasks.map(({ id, title, completed }) => (
-          <li key={id}>
-            <input
-              type="checkbox"
-              checked={completed}
-              onChange={() => toggleComplete(id)}
-              style={{ marginRight: 10 }}
-            />
-            <Link to={`/tasks/${id}`} style={{ textDecoration: completed ? 'line-through' : 'none' }}>
-              {title}
-            </Link>
-            <button onClick={() => deleteTask(id)} style={{ marginLeft: 10 }}>
+          <li key={id} className="task-item">
+            <label>
+              <input
+                type="checkbox"
+                checked={completed}
+                onChange={() => toggleComplete(id)}
+                aria-label={`Mark task ${title} as completed`}
+                className="task-checkbox"
+              />
+              <Link
+                to={`/tasks/${id}`}
+                className={`task-link ${completed ? 'completed' : ''}`}
+              >
+                {title}
+              </Link>
+            </label>
+            <button
+              onClick={() => deleteTask(id)}
+              className="delete-task-button"
+              aria-label={`Delete task ${title}`}
+            >
               Delete
             </button>
           </li>
@@ -45,4 +55,4 @@ const TaskDashboard = () => {
   )
 }
 
-export default TaskDashboard;
+export default TaskDashboard
